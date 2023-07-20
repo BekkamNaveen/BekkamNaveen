77cd6d23338991ed1e24fd8cad1f363fb5bdcc7a
BekkamNaveen <bekkamnaveen850@gmail.com>
2023-07-20 10:45:43 +0530

    commited by naveen

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
